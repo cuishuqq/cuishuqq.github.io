49b7b05d191e5b3893d5ccdac31e93e1412f0231
cuig282-glitch <cuig282@gmail.com>
2025-11-01 21:01:01 +0800

    Site updated: 2025-11-01 21:01:01

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/26/lingo使用指南/","2025/10/23/hexo使用技巧/","2025/10/23/半小时代码处理/","2025/10/26/六级备考/"];function toRandomPost(){
+var posts=["2025/10/23/hexo使用技巧/","2025/10/29/linux常用指令/","2025/10/26/lingo使用指南/","2025/10/26/六级备考/","2025/10/23/天气数据分析处理/","2025/10/28/气象局使用注意事项/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
